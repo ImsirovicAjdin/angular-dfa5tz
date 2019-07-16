d6d3e529481944b7482e57f2e99856b3ddc1b5c5
ImsirovicAjdin <ajdika@live.com>
2019-07-16 14:49:49 +0200

    Remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,21 +23,13 @@ export class AppComponent  {
   constructor() {
     this.clock = Observable.merge(
       this.click$,
-      Observable.interval(1000) // Now I'll change it back to 1s to make it more natural
+      Observable.interval(1000)
     )
       .startWith(new Date())
-      //.scan((acc, curr)=> acc);
-      /*
-      So in order to update this, I need to say that when something comes through, I want to grab a copy, so I'll say date is a new Date, and I just wanna grab the accumulator getTime, and then return that date
-       // so now, since we have a copy of the date, it's going to behave exactly the same as before - it'll remain static, since it's just returning that copy. 
-
-       But what we can do is take that date and change it! (*)
-      */
       .scan((acc, curr)=> {
         const date = new Date(acc.getTime());
 
-        date.setSeconds(date.getSeconds() + 1); // (*) so we'll say date.setSeconds, getSeconds and add 1
-        // so now it's gonna update every 5 seconds, but it's also gonna update on click.
+        date.setSeconds(date.getSeconds() + 1);
 
         return date;
       })
