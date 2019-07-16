a1e0f1e8c3b0f93e70c3392f393abef3595cfb55
ImsirovicAjdin <ajdika@live.com>
2019-07-16 14:49:17 +0200

    I'll grab a copy of the date and now I can edit the copy!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,5 @@
 // 5 Manage State in RxJS with StartWith and Scan
 
-/*
-Since I want to control the clock like move the time forward or backward or whatever, 
-right now I'm just pushing a new Date every 5 seconds or every time I click, what I need to do is actually track a date, and then make changes to that date. 
-
-When I want to track someting in RxJS I wanna .startWith an initial value (new Date()),
-and then every time some sort of event comes through, I'm gonna check for it -- .scan() --, and then change it.
-
-That new date is that acc (accumulator), and then I can use the curr (current), the thing that comes through, to change the accumulator however I want. So to start with, I'm just going to return the accumulator to see what happens. It's just gonna return out that same value. The clock is no longer updating, it's just set to that initial value.
-*/
-
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
@@ -30,20 +20,26 @@ export class AppComponent  {
   click$ = new Subject();
   clock;
 
-  /*
-  constructor(){
-    this.clock = Observable.merge(
-        this.click$,
-        Observable.interval(5000)
-    ).map(()=> new Date());
-  }
-  */
   constructor() {
     this.clock = Observable.merge(
       this.click$,
-      Observable.interval(5000)
+      Observable.interval(1000) // Now I'll change it back to 1s to make it more natural
     )
       .startWith(new Date())
-      .scan((acc, curr)=> acc);
+      //.scan((acc, curr)=> acc);
+      /*
+      So in order to update this, I need to say that when something comes through, I want to grab a copy, so I'll say date is a new Date, and I just wanna grab the accumulator getTime, and then return that date
+       // so now, since we have a copy of the date, it's going to behave exactly the same as before - it'll remain static, since it's just returning that copy. 
+
+       But what we can do is take that date and change it! (*)
+      */
+      .scan((acc, curr)=> {
+        const date = new Date(acc.getTime());
+
+        date.setSeconds(date.getSeconds() + 1); // (*) so we'll say date.setSeconds, getSeconds and add 1
+        // so now it's gonna update every 5 seconds, but it's also gonna update on click.
+
+        return date;
+      })
   }
 }
